564111f9b237c9e3884ccec154918e07caa8cdff
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-02 13:32:15 +0300

    Update 0-making_change: Optimize and refactor algorithm

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,40 +12,29 @@ def makeChange(coins: list, total: int) -> int:
     """
     # Check if the coins list is None or empty or
     # the total change amount is equal to or less than 0
-    if not coins or total <= 0:
+    if not isinstance(coins, list) or len(coins) == 0 or total <= 0:
         return 0
 
-    # check if our coins variable is iterable
-    # i.e instance of (list, tuple, set, sequence, iterable)
-    try:
-        coins = list(iter(coins))
-    except BaseException:
-        return -1
-
-    # sort the denomination from the largest to the smallest
-    coins = sorted(coins, reverse=True)
+    # sort the denomination from the largest to the smallest in place
+    coins.sort(reverse=True)
 
     # Create our change count variable
     count: int = 0
 
     # print("{0} => {1}".format(coins, total))
     # Iterate the list of denominations to find total change count
-    while True:
-        get_change: bool = False
-        for den in coins:
-            if den <= total:
-                get_change = True
-                change_count: int = (total // den)
-                # print("{0} - {1} = {2} \t\t: +{3}"
-                #       .format(total,
-                #               den * change_count,
-                #               total - (den * change_count),
-                #               change_count)
-                #       )
-                total -= (den * change_count)
-                count += change_count
-        if not get_change:
-            break
+    for den in coins:
+        if den <= total:
+            change_count: int = (total // den)
+            # print("{0} - {1} = {2} \t\t: +{3}"
+            #       .format(total,
+            #               den * change_count,
+            #               total - (den * change_count),
+            #               change_count)
+            #       )
+            total -= (den * change_count)
+            count += change_count
+
     # if could not get change for the total amount return Failed
     if total != 0:
         return -1
