ba9a3e3bf412b3f13565d72011a5b8c0a8f49874
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-18 11:12:17 +0300

    Update 0-lockboxes: Fix index out of range exception && update code comments

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Algoritms to check if a number of locked boxed can all be opened
-given each box numbered sequenctially from 0 to n - 1 and each box may
+Algorithm to check if a number of locked boxed can all be opened
+given each box numbered sequentially from 0 to n - 1 and each box may
 contain keys to other boxes
 """
 
@@ -20,28 +20,47 @@ class Box:
 
 def canUnlockAll(boxes: list) -> bool:
     """Returns True if all boxes can be opened, otherwise return False"""
+    if not isinstance(boxes, list) or len(boxes) == 0:
+        return None
+
+    # Create our boxes
     my_boxes = [Box(index, boxes[index]) for index in range(len(boxes))]
-    boxNum = 0
-    my_keys = set()
+    # Start with the first opened box
+    boxIndex = 0
+    # collection of all keys
+    usable_keys = set()
+    # collection of all used keys
     used_keys = set()
     try:
         while True:
-            if boxNum not in used_keys:
-                curr_box = my_boxes[boxNum]
+            # Open the box if not already opened as the key matches the box
+            curr_box = None
+            if boxIndex not in used_keys:
+                # Check if the key can be used to open a box
+                if boxIndex < len(my_boxes):
+                    curr_box = my_boxes[boxIndex]
+                # if the key can't open a box discard the key and try another
+                # one
                 if curr_box is None:
-                    my_keys.discard(boxNum)
-                    boxNum = next(iter(my_keys))
+                    usable_keys.discard(boxIndex)
+                    boxIndex = next(iter(usable_keys))
                     continue
-                my_keys.update(curr_box.keys)
+                # Otherwise add it to our used keys and get all the keys
+                # inside the box to add to our usable keys collection
+                # while also setting the box as visited
+                usable_keys.update(curr_box.keys)
+                used_keys.add(boxIndex)
                 curr_box.visited = True
-                used_keys.add(boxNum)
-            if boxNum in my_keys and boxNum in used_keys:
-                my_keys.discard(boxNum)
-            if len(my_keys) == 0:
+            # If key is already added to the used collection
+            # remove it from usable keys
+            if boxIndex in usable_keys and boxIndex in used_keys:
+                usable_keys.discard(boxIndex)
+            # if there are no more keys to use
+            if len(usable_keys) == 0:
                 break
-            boxNum = next(iter(my_keys))
-        visited_boxes = filter(lambda x: x.visited is True, my_boxes)
-        # [print((b.index, b.keys, b.visited)) for b in visited_boxes]
-        return True if len(used_keys) == len(my_boxes) else False
-    except BaseException:
-        raise
+            # otherwise get the next key to try out
+            boxIndex = next(iter(usable_keys))
+    except StopIteration:
+        pass
+    [print((b.index, b.keys, b.visited)) for b in my_boxes]
+    return True if len(used_keys) == len(my_boxes) else False
