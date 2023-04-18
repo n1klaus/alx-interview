345983e450f2c5814e3dc8cb9391c3aad14168b7
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-18 10:11:47 +0300

    Update 0-pascal_triangle: remove type annotations and add more code comments

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,34 +2,37 @@
 """Solving pascal's triangle challenge """
 
 
-def pascal_triangle(n: int):
+def pascal_triangle(n):
     """Function to create a pascal's triangle
 
     Args:
-        n (int): number of rows in the triangle
+        n (int): number of rowIndexs in the triangle
 
     Returns:
         triangle (list): a list of list of integers making up the triangle
 
     """
-    triangle: list = []
+    triangle = []
     if n <= 0:
         return triangle
 
-    try:
-        for row in range(n):
-            # create a list of integers for every row
-            new_row = [None] * (row + 1)
-            new_row[0] = 1
-            if row >= 2:
-                # Add values which fall in range of 1 to -2 if there are any
-                for col in range(1, n - 1):
-                    if col < row:
-                        new_row[col] = triangle[row - 1][col] + \
-                            triangle[row - 1][col - 1]
-            if n >= 2:
-                new_row[-1] = 1
-            triangle.append(new_row)
-        return triangle
-    except BaseException:
-        raise
+    for rowIndex in range(n):
+        # fill the row with empty values
+        new_row = [None] * (rowIndex + 1)
+        # Insert 1 in first index
+        new_row[0] = 1
+        # Check if there are possible indexes between the first and last index
+        if rowIndex >= 2:
+            # Fill values which fall in between the first and last index
+            for colIndex in range(1, n - 1):
+                if colIndex < rowIndex:
+                    new_row[colIndex] = triangle[rowIndex - 1][colIndex] + \
+                        triangle[rowIndex - 1][colIndex - 1]
+                else:
+                    break
+        # Insert 1 in last index if the rowIndex size is greater than 1
+        if n >= 2:
+            new_row[-1] = 1
+        # finally add the created rowIndex into the triangle
+        triangle.append(new_row)
+    return triangle
